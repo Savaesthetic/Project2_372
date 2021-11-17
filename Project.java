1ff4731e9fd5e54eb7510c9a2acc709b7358c5ca
Savaesthetic <asavaus@gmail.com>
2021-11-17 02:39:41 -0700

    Flushed out var and print, started conditional.

--- Project.java
+++ Project.java
@@ -7,30 +7,26 @@ import java.util.regex.Pattern;
 
 public class Project {
     static HashMap<String, Integer> variables = new HashMap<String, Integer>();
-    public static void main(String[] args){
+    public static void main(String[] args) {
         try {
-            String program = Files.readString(Path.of("assignment.txt"));
+            String program = Files.readString(Path.of("conditional.txt"));
             String[] statements = program.split("\\?");
-            for (String statement: statements){
-                Pattern prefix = Pattern.compile("var|loop|if|console");
+
+            for (String statement: statements) {
+                Pattern prefix = Pattern.compile("var|loop|do|console");
                 Matcher matcher = prefix.matcher(statement);
-                // System.out.println(" beginning ");
-                // System.out.println("tostring " + matcher.toString());
-                // System.out.println("pattern " + matcher.pattern());
-                // System.out.println("matches " + matcher.matches());
-                // System.out.println("find " + matcher.find());
-                // System.out.println("group2 " + matcher.group());
+
                 if (matcher.find()) {
-                    // todo deal with found statement
-                    switch (matcher.group()) {
+                    switch (matcher.group()) { // TODO CHECK FOR ERRORS THROUGH RETURN AND EXIT OR EXIT IN FUNCTIONS?
                         case "var":
                             dealWithVar(statement);
                             break;
                         case "loop":
+                            // TODO Deal with Loop Statement
                             // dealWithLoop(statement);
                             break;
-                        case "if":
-                            System.out.println("found loop");
+                        case "do":
+                            dealWithConditional(statement);
                             break;
                         case "console":
                             dealWithPrint(statement);
@@ -38,28 +34,24 @@ public class Project {
                     }
                 } else {
                     System.out.println("Not a valid statement: " + statement);
-                    // TODO compilation error
+                    // TODO IS THIS ENOUGH TO CONSTITUE A COMPILATION ERROR?
                 }
             }
         } catch (IOException e) {
-            System.out.println("Error");
+            System.out.println("Error Finding File Path.");
             e.printStackTrace();
         }
     }
 
-    static void dealWithVar(String statement) 
-    {
-        Pattern pattern = Pattern.compile("var [A-Z]+:"); // "var [A-Z]+:[0|1]+$"
+    static void dealWithVar(String statement) {
+        Pattern pattern = Pattern.compile("var [A-Z]+:");
         Matcher matcher = pattern.matcher(statement);
         if (matcher.find()) {
             pattern = Pattern.compile("^[0|1]+$");
             matcher = pattern.matcher(statement.split(":")[1]);
             if (matcher.find()) {
                 String[] var = statement.split(" |:"); // name = var[1], val = var[2]
-                // TODO PARSE BINARY INTO DECIMAL AND ADD TO HASHMAP
-                Integer value = Integer.parseInt(var[2], 2); // convert from binary string to integer
-                variables.put(var[1], value);
-                System.out.println("Valid statement: " + statement);
+                variables.put(var[1], Integer.parseInt(var[2], 2)); // Convert from binary to decimal and add to dictionary
             } else {
                 // TODO COMPILATION ERROR
                 System.out.println("Variables can only be assigned to binary values: " + statement);
@@ -70,42 +62,54 @@ public class Project {
         }
     }
 
-    // static void dealWithLoop(String statement)
-    // {
-    //     Pattern prefix = Pattern.compile("loop <[A-Z]+->[0|1]+, ?[0|1]+> {(.*)}$");
-    //     Matcher matcher = prefix.matcher(statement);
-    //     System.out.println(statement);
-    //     System.out.println("find " + matcher.find());
-    // }
+    static void dealWithLoop(String statement) {
+        Pattern prefix = Pattern.compile("loop <([A-Z]+|[0|1]+)->([A-Z]+|[0|1]+), ?[0|1]+> ?{.*}$"); // [A-Z]+->[0|1]+, ?[0|1]+
+        Matcher matcher = prefix.matcher(statement);
+        System.out.println(statement);
+        System.out.println("find " + matcher.find());
+    }
 
-    static void dealWithPrint(String statement) 
-    {
-        //Pattern prefix = Pattern.compile("console <[A-Z]+|[1|0]+|'.*'>$"); // is there a way to tell hich or I'm matching with
+    static void dealWithPrint(String statement) {
         Pattern  pattern = Pattern.compile("console <.*>$");
         Matcher matcher = pattern.matcher(statement);
-        // can split up into console <.*>$ and split strip < > and then do a regex for [A-Z]+|[1|0]+|'.*' for more in depth error checking
-        if (matcher.matches()) {
-            Pattern val = Pattern.compile("[1|0]?");
+        if (matcher.find()) {
+            Pattern val = Pattern.compile("[1|0]+");
             Pattern literal = Pattern.compile("'.*'");
             Pattern var = Pattern.compile("[A-Z]+");
-            matcher = pattern.matcher(statement.split("<|>")[1]);
-            if (val.matcher(statement.split("<|>")[1]).matches()) { // might have to seperate into multiple ifs and patterns
-                // TODO DEAL WITH VAL
-                System.out.println("Valid print statement: " + statement);
-            } else if (literal.matcher(statement.split("<|>")[1]).matches()){
-                // TODO Deal with literal
-            } else if (var.matcher(statement.split("<|>")[1]).matches()) {
-                System.out.println("Can only output one variable, value, or string literal at a time: " + statement);
-            } else {
 
+            String[] printStatement = statement.split("<|>");
+            if (printStatement.length > 1) { // Checks to see if trying to print nothing
+                if (val.matcher(printStatement[1]).matches()) {
+                    System.out.println(Integer.parseInt(printStatement[1], 2)); // Convert to binary and print
+                } else if (literal.matcher(printStatement[1]).matches()) {
+                    System.out.print(printStatement[1].subSequence(1, printStatement[1].length()-1)); // Print literal stripped of surrounding ''
+                } else if (var.matcher(printStatement[1]).matches()) {
+                    Integer varVal;
+                    if ((varVal = variables.get(printStatement[1])) != null) {
+                        System.out.println(varVal);
+                    } else {
+                        System.out.println("Unable to print variable that does not exist: " + statement);
+                    }
+                } else {
+                    // TODO COMPILATION ERROR TOO BROAD?
+                    System.out.println("Can only output one variable, value, or string literal at a time: " + statement);
+                }
             }
         } else {
             // TODO DEAL WITH COMPILATION ERROR
-            System.out.println("Must use output <> to print to console: " + statement);
+            System.out.println("Must use console <> to print to console: " + statement);
         }
     }
 
     static void dealWithConditional(String statement) {
-        // TODO CREATE CONDITION REGEX CHECKER
+        // TODO CREATE CONDITION CHECKER
+        Pattern prefix = Pattern.compile("do <.*> if <.*> otherwise do <.*>$"); // [A-Z]+->[0|1]+, ?[0|1]+
+        Matcher matcher = prefix.matcher(statement);
+        if (matcher.find()) {
+            System.out.println("Valid conditional: " + statement);
+            // TODO GO FURTHER IN
+        } else {
+            System.out.println("Conditional must be done in the form do <.*> if <.*> otherwise do <.*>: " + statement);
+        }
     }
 }
